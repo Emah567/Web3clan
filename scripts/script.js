e971b69195f4ea612e0e776ceada549196386288
* <edetemmanuel567@gmail.com>
2024-07-02 23:09:05 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/6EOTEhhgDbtwn5dj/scene.splinecode');
